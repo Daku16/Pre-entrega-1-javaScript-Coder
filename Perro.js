a7d849dffcf8ebd03d15ad48cf4a001269c8010e
David Gutierrez <1608davidg@gmail.com>
2023-04-03 16:11:38 -0500

    Segundo commit

---
+++ Perro.js
@@ -0,0 +1,41 @@
+class Perro {
+    constructor(precio) {
+        this.nombre = ""
+        this.raza = ""
+        this.edad = 0
+        this.motivo = ""
+        this.precio = 150000
+        this.cita = ""
+        this.medicamento = []
+        this.precioMedicamento = [80000, 50000, 25000, 60000, 30000]
+        this.factura = 0
+    }
+    agregar() {
+        this.nombre = prompt("Ingrese el nombre de la mascota").toLowerCase()
+        this.raza = prompt("Ingrese la raza de la mascota")
+        this.edad = prompt("Ingrese la edad de la mascota")
+        this.motivo = prompt("Ingrese el motivo del ingreso")
+        return alert("Se ha ingresado la mascota")
+    }
+    agendarCita(anio, mes, dia) {
+        let agenda = new Date(anio, mes - 1, dia)
+        alert("!Se ha creado la cita el dia: " + agenda.toLocaleDateString())
+        this.cita = agenda
+    }
+    asignarMedicina(opcion) {
+        for (let i = 0; i < this.precioMedicamento.length; i++) {
+            if (opcion === i) {
+                this.medicamento.push(this.precioMedicamento[opcion - 1])
+                alert("Se ha asignado el medicamento")
+                break
+            }
+        }
+    }
+    generarFactura() {
+        for (let i = 0; i < this.medicamento.length; i++) {
+            this.factura += this.medicamento[i]
+        }
+        this.factura = this.precio + this.factura
+        alert("El costo total es de: " + this.factura)
+    }
+}
--- script.js
+++ script.js
@@ -1,49 +1,109 @@
-let contador = 0
-let contadorG = 0
-let contadorP = 0
-let contadorR = 0
 let menu = true
+let mascotas = []
 
-function agregar() {
-    let nombre = prompt("Ingrese el nombre de la mascota")
-    let raza = prompt("Ingrese la raza de la mascota")
-    let edad = prompt("Ingrese la edad de la mascota")
-    let motivo = prompt("Ingrese el motivo del ingreso")
-    return alert("Se ha ingresado la mascota")
-}
 alert("Binevenido a la veterinaria!")
 
 do {
-    let opcion = parseInt(prompt("Que tipo de mascota desea agregar? \n1) Gato - Costo 100.000$ \n2) Perro - Costo 150.000$ \n3) Reptil - Costo 300.000$ \n4) Salir"))
+    let opcion = parseInt(prompt("Que tipo de mascota desea agregar? \n1) Gato - Costo 100.000$ \n2) Perro - Costo 150.000$ \n3) Reptil - Costo 300.000$ \n4) Cancelar"))
     if (opcion === 1) {
-        agregar()
-        contador++
-        contadorG++
+        const g = new Gato(100000)
+        g.agregar()
+        mascotas.push(g)
     } else if (opcion === 2) {
-        agregar()
-        contador++
-        contadorP++
+        const p = new Perro(150000)
+        p.agregar()
+        mascotas.push(p)
     } else if (opcion === 3) {
-        agregar()
-        contador++
-        contadorR++
+        const r = new Reptil(300000)
+        r.agregar()
+        mascotas.push(r)
     } else if (opcion === 4) {
         menu = false
-        alert("Saliendo")
+        alert("Cancelando")
     } else {
         alert("Opcion invalida")
     }
-    let menu2 = true
-    while (menu2) {
+    if (opcion !== 4) {
         let opcion2 = parseInt(prompt("Deseas agregar otra mascota?\n1) Si\n2) No"))
         if (opcion2 === 1) {
-            menu2 = false
+            menu = true
         } else if (opcion2 === 2) {
-            menu2 = false
             menu = false
         } else {
             alert("Opcion invalida")
         }
     }
 } while (menu)
-alert("La factura total de los animales ingresados es de: " + ((contadorG*100000)+(contadorP*150000)+(contadorR*300000)))
+
+console.log(mascotas.filter(mascota =>  {
+    return mascota.nombre
+}))
+let menu2 = true
+
+while (menu2) {
+    let opcion2 = parseInt(prompt("Que desea hacer? \n1) Ver listado de mascotas \n2) Agendar cita \n3) Solicitar medicamento \n4) Calcular costos \n5) Salir"))
+    switch (opcion2) {
+        case 1:
+            let opcionFiltro = parseInt(prompt("Desea filtar por el tipo de animal? \n1) Si\n2) No"))
+            if (opcionFiltro === 1) {
+                let filtro = parseInt(prompt("Opciones: \n1) Gato \n2) Perro \n3) Reptil"))
+                if (filtro === 1) {
+                    let gatos = mascotas.filter(mascota => mascota.precio == 100000)
+                    alert("GATOS \n"+gatos.map(gato => {
+                        return "- " + gato.nombre + "\n"
+                    }).join(''))
+                } else if (filtro === 2) {
+                    let perros = mascotas.filter(mascota => mascota.precio == 150000)
+                    alert("PERROS \n"+perros.map(perro => {
+                        return "- " + perro.nombre + "\n"
+                    }).join(''))
+                } else if (filtro === 3) {
+                    let reptiles = mascotas.filter(mascota => mascota.precio == 300000)
+                    alert("REPTILES \n"+reptiles.map(reptil => {
+                        return "- " + reptil.nombre + "\n"
+                    }).join(''))
+                } else {
+                    alert("Opcion invalida")
+                }
+            } else if (opcionFiltro === 2) {
+                alert(mascotas.map(mascota => {
+                    return "- " + mascota.nombre + "\n"
+                }).join(''))
+            } else {
+                alert("Opcion invalida")
+            }
+            break
+        case 2:
+            let nombreCita = prompt("Ingrese el nombre de la mascota a la que desea agendar la cita").toLowerCase()
+            let cita = mascotas.find(mascota => mascota.nombre === nombreCita)
+            let anio = prompt("Ingrese el año de la cita")
+            let mes = prompt("Ingrese el mes de la cita")
+            let dia = prompt("Ingrese el dia de la cita")
+            cita.agendarCita(anio, mes, dia)
+            break
+        case 3:
+            let nombreMedicina = prompt("Ingrese el nombre de la mascota a la que desea asignar el medicamento").toLowerCase()
+            let medicina = mascotas.find(mascota => mascota.nombre === nombreMedicina)
+            let opcion = parseInt(prompt("Que medicamento deseas apartar? \n1) Antipulgas - 80000$ \n2) Antiparasitario 50000$ \n3) Suplemento Alimenticio 25000$\n4) Solución Oral 60000$\n5) Vitaminas 30000$"))
+            if(opcion >= 0 && opcion <= 5) {
+                medicina.asignarMedicina(opcion)
+                for (const mascota of mascotas) {
+                    console.log(mascota)
+                }
+            } else {
+                alert("Opcion invalida")
+            }
+            break
+        case 4:
+            let nombreFactura = prompt("Ingrese el nombre de la mascota a la que desea generar la factura").toLowerCase()
+            let factura = mascotas.find(mascota => mascota.nombre === nombreFactura)
+            factura.generarFactura()
+            break
+        case 5:
+            menu2 = false
+            alert("Saliendo!")
+            break
+        default:
+            alert("Opcion invalida")
+    }
+}
